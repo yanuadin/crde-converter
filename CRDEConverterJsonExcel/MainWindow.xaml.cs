5d0a1d45eaf0796a9c584888f2f5bb03666d77f5
Yanu <yanu30adi@gmail.com>
2025-03-06 16:47:03 +0700

    dynamic control tab menu

--- CRDEConverterJsonExcel/MainWindow.xaml.cs
+++ CRDEConverterJsonExcel/MainWindow.xaml.cs
@@ -35,11 +35,15 @@ using System.Data;
 using System.Net;
 using CRDEConverterJsonExcel.objectClass;
 using System.Security.Cryptography;
+using System.Collections.ObjectModel;
+using CRDEConverterJsonExcel.src.tools;
+using CRDEConverterJsonExcel.src.setting;
 
 namespace CRDEConverterJsonExcel;
 
 public partial class MainWindow : Window
 {
+    ObservableCollection<TabItemControl> tabItemControls = new ObservableCollection<TabItemControl>();
     public MainWindow()
     {
         InitializeComponent();
@@ -53,53 +57,78 @@ public partial class MainWindow : Window
         switch (menuItem.Tag)
         {
             case "t1":
-                toolsSubMenu.Visibility = Visibility.Visible;
-                settingSubMenu.Visibility = Visibility.Hidden;
+                addTabItem(new TabItemControl { Header = "JSON Converter", Content = new JSONConverter() });
                 break;
             case "t2":
-                toolsSubMenu.Visibility = Visibility.Visible;
-                settingSubMenu.Visibility = Visibility.Hidden;
+                addTabItem(new TabItemControl { Header = "Excel Converter", Content = new ExcelConverter() });
                 break;
             case "t3":
-                toolsSubMenu.Visibility = Visibility.Visible;
-                settingSubMenu.Visibility = Visibility.Hidden;
+                addTabItem(new TabItemControl { Header = "JSON Masking", Content = new UserControl() });
                 break;
             case "t4":
-                toolsSubMenu.Visibility = Visibility.Visible;
-                settingSubMenu.Visibility = Visibility.Hidden;
-                t4_uc_S1LogExtractionLocal.refreshConfig();
+                addTabItem(new TabItemControl { Header = "S1 Log Extraction", Content = new S1LogExtractionLocal() });
+                refreshAllComponentConfig();
                 break;
             case "t5":
-                toolsSubMenu.Visibility = Visibility.Visible;
-                settingSubMenu.Visibility = Visibility.Hidden;
-                t4_uc_CallingCRDEAPI.refreshConfig();
+                addTabItem(new TabItemControl { Header = "Calling CRDE API", Content = new CallingCRDEAPI() });
+                refreshAllComponentConfig();
                 break;
             case "s1":
-                settingSubMenu.Visibility = Visibility.Visible;
-                toolsSubMenu.Visibility = Visibility.Hidden;
+                addTabItem(new TabItemControl { Header = "Masking", Content = new UserControl() });
                 break;
             case "s2":
-                settingSubMenu.Visibility = Visibility.Visible;
-                toolsSubMenu.Visibility = Visibility.Hidden;
+                addTabItem(new TabItemControl { Header = "S1 Logs", Content = new UserControl() });
                 break;
             case "s3":
-                settingSubMenu.Visibility = Visibility.Visible;
-                toolsSubMenu.Visibility = Visibility.Hidden;
+                addTabItem(new TabItemControl { Header = "CRDE API Address", Content = new CRDEAPIAddressSetting() });
                 break;
             case "s4":
-                settingSubMenu.Visibility = Visibility.Visible;
-                toolsSubMenu.Visibility = Visibility.Hidden;
+                addTabItem(new TabItemControl { Header = "Process Code", Content = new ProcessCodeSetting() });
                 break;
             default:
                 MessageBox.Show("[ERROR]: Menu is not available");
                 break;
         }
+
+        subMenu.ItemsSource = tabItemControls;
+    }
+
+    private void btn_DeleteTabClick(object sender, RoutedEventArgs e)
+    {
+        Button button = sender as Button;
+        TabItemControl tabItemControl = button.DataContext as TabItemControl;
+        if (tabItemControl != null)
+            tabItemControls.Remove(tabItemControl);
+
+        subMenu.ItemsSource = tabItemControls;
+    }
+
+    private void addTabItem(TabItemControl itemControl)
+    {
+        if (tabItemControls.FirstOrDefault(item => item.Header == itemControl.Header) == null)
+        {
+            tabItemControls.Add(itemControl);
+            subMenu.SelectedItem = itemControl;
+        }
+
+        subMenu.ItemsSource = tabItemControls;
     }
 
     private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
     {
+        refreshAllComponentConfig();
+    }
+
+    private void refreshAllComponentConfig()
+    {
         // Refreshing user control that use config
-        t4_uc_S1LogExtractionLocal.refreshConfig();
-        t4_uc_CallingCRDEAPI.refreshConfig();
+        foreach (TabItemControl itemControl in tabItemControls)
+        {
+            var method = itemControl.Content.GetType().GetMethod("refreshConfig");
+            if (method != null)
+            {
+                method.Invoke(itemControl.Content, null);
+            }
+        }
     }
 }
